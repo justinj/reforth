cc1ea0636992ff0b778468fbdbd1c163dfd424e0
Justin Jaffray <justin.jaffray@gmail.com>
2013-02-26 08:52:48 -0500

    added IF ELSE THEN

--- lib/conditional.rb
+++ lib/conditional.rb
@@ -10,5 +10,29 @@ module Forth
       b = pop
       push (a == b) ? 1 : 0
     end
+
+    def_word :IF do
+      a = pop
+      if a == 0
+        depth = 1
+        until matching_then(depth) || matching_else(depth)
+          pointer_inc
+          depth += 1 if token_under_pointer == "IF"
+          depth -= 1 if token_under_pointer == "THEN"
+        end
+      end
+    end
+
+    def matching_then depth
+      depth == 0 && token_under_pointer == "THEN"
+    end
+
+    def matching_else depth
+      depth == 1 && token_under_pointer == "ELSE"
+    end
+
+    def_word :THEN do
+
+    end
   end
 end
--- spec/conditional_spec.rb
+++ spec/conditional_spec.rb
@@ -12,4 +12,24 @@ describe Forth::Conditional do
       parser.run("5 6 = .").should eql "0"
     end
   end
+
+  describe "#IF" do
+    it "executes up to THEN if the thing on top of the stack is truthy" do
+      parser.run("1 IF 5 . THEN").should eql "5"
+    end
+
+    it "doesn't execute up to the THEN if the thing on top of the stack is falsey" do
+      parser.run("0 IF 5 . THEN").should eql ""
+    end
+
+    it "can be nested" do
+      parser.run("0 IF 1 IF THEN 5 . THEN").should eql ""
+    end
+  end
+
+  describe "#ELSE" do
+    it "is executed if an IF is not" do
+      parser.run("0 IF 1 . ELSE 2 . THEN").should eql "2"
+    end
+  end
 end
