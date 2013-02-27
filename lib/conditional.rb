19efccfaa23bbb901e970d95dd074974766f4866
Justin Jaffray <justin.jaffray@gmail.com>
2013-02-27 08:13:07 -0500

    Added helper methods for truthiness

--- lib/conditional.rb
+++ lib/conditional.rb
@@ -5,35 +5,43 @@ require_relative "forth_module"
 module Forth
   module Conditional
     include ForthModule
+    def truthify(value)
+      value ? 1 : 0 
+    end
+
+    def truthy?(value)
+      value != 0
+    end
+
     def_word :"=" do
       a = pop
       b = pop
-      push (a == b) ? 1 : 0
+      push truthify(a == b)
     end
 
     def_word :">" do
       a = pop
       b = pop
-      push (b > a) ? 1 : 0
+      push truthify(b > a)
     end
 
     def_word :"<" do
       a = pop
       b = pop
-      push (b < a) ? 1 : 0
+      push truthify(b < a)
     end
 
     def_word :OR do
       a = pop
       b = pop
-      push ((a != 0) || (b != 0)) ? 1 : 0
+      push truthify(truthy?(a) || truthy?(b))
     end
 
     def_word :IF do
       a = pop
       pointer_inc
       block = token_under_pointer
-      if a != 0
+      if truthy?(a)
         run_block block[0]
       elsif block.count > 1
         run_block block[1]
